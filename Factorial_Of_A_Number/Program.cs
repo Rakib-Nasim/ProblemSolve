4235fe4ea18482a67dedc4d299bb674b5d9cb09e
Rakibul Hasan Nasim <hasan@vonome.com>
2022-12-04 17:23:18 +0600

    SoloLearnPblm

---
+++ Factorial_Of_A_Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial_Of_A_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Give Your Number....");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+    
+}
---
+++ UserType_PassInConstructor_SoloLearnPblm/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UserType_PassInConstructor_SoloLearnPblm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            int points1 = Convert.ToInt32(Console.ReadLine());
+            string name2 = Console.ReadLine();
+            int points2 = Convert.ToInt32(Console.ReadLine());
+
+            DancerPoints dancer1 = new DancerPoints(name1, points1);
+            DancerPoints dancer2 = new DancerPoints(name2, points2);
+
+            DancerPoints total = new DancerPoints(dancer1, dancer2);
+            Console.WriteLine(total.name);
+            Console.WriteLine(total.points);
+        }
+    }
+    class DancerPoints
+    {
+        public string name;
+        public int points;
+        public DancerPoints(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public DancerPoints(DancerPoints dencer1, DancerPoints dancer2)
+        {
+            this.name = dencer1.name + " & " + dancer2.name;
+            this.points = dencer1.points + dancer2.points;
+        }
+
+    }
+}
