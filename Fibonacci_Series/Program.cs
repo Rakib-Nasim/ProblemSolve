d733f7415c096c312fb0d9753020e80b521882b2
Rakibul Hasan Nasim <hasan@vonome.com>
2022-12-08 14:00:35 +0600

    Fibonacci Series

---
+++ Fibonacci_Series/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fibonacci_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start1=0, start2=1, total =0;
+            Console.WriteLine("Please Set The Limite!");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < limit-2; i++)
+            {
+                if (start1 == 0 && start2 == 1)
+                {
+                    Console.Write(start1 + " " + start2);
+                }
+                total = start1+start2 ;
+                start1 = start2;
+                start2 = total;  
+                if (total < 0)
+                {
+                    Console.WriteLine(" "+"Negative Number");
+                    break;
+                }
+                Console.Write(" "+total);
+            }
+            Console.ReadKey();
+        }
+    }
+}
