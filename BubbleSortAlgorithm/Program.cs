5379502e7a484cf3e69b58ab1e9b003072678e13
Rakibul Hasan Nasim <hasan@vonome.com>
2022-12-13 16:20:19 +0600

    Bubble Sort Algorithm

---
+++ BubbleSortAlgorithm/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BubbleSortAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbersArr = { 8, 2, 5, 10, 9, 7, 6, 4, 1, 3 };
+                for (int j = 0; j < numbersArr.Length-1; j++)
+                {
+                    for (int i = 0; i < numbersArr.Length - 1; i++)
+                    {
+                        int number1 = 0;
+                        int number2 = 0;
+                        if (numbersArr[i] > numbersArr[i + 1])
+                        {
+                            number1 = numbersArr[i];
+                            number2 = numbersArr[i + 1];
+                            numbersArr[i] = number2;
+                            numbersArr[i + 1] = number1;
+                        }
+                    }
+
+                }
+                
+                foreach (int item in numbersArr)
+                {
+                    Console.Write(" " + item);
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+        
+    }
+}
