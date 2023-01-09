7105b462d3b3963118ec70359f193ac8ec46b23c
Rakibul Hasan Nasim <76495731+Rakibul7828@users.noreply.github.com>
2023-01-09 08:20:08 +0600

    Armstrong number solve

--- Armstrong_Number/Program.cs
+++ Armstrong_Number/Program.cs
@@ -8,24 +8,23 @@ namespace Armstrong_Number
         {
             Console.WriteLine("Enter Your Number");
             int number = Convert.ToInt32(Console.ReadLine());
-            int remainder = 0, reverse = number,total=0;
+            int remainder = 0, reverse = number,total=0,powr;
+            string lengthCount = number.ToString();
             while (reverse != 0)
             {
                 remainder = reverse % 10;
                 reverse = reverse / 10;
-                total = total + (remainder * remainder * remainder);
+                powr = lengthCount.Length;
+                total = total + Convert.ToInt32((Math.Pow(remainder,powr)));
             }
             if (number == total)
             {
-                Console.WriteLine("Is Armstrong {0}", number);
+                Console.WriteLine("{0} Is Armstrong ", number);
             }
             else
             {
-                Console.WriteLine("Is Not Armstrong {0}", number);
+                Console.WriteLine("{0} Is Not Armstrong ", number);
             }
-
-
-
         }
     }
 }
