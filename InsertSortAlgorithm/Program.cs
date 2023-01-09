8778135a690c75a13f4456f1e963890c302f239f
Rakibul Hasan Nasim <76495731+Rakibul7828@users.noreply.github.com>
2023-01-09 08:54:45 +0600

    Insert Sort Algorithm

---
+++ InsertSortAlgorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InsertSortAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] numbers ={ 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (numbers[j - 1] > numbers[j])
+                    {
+                        int temp = numbers[j - 1];
+                        numbers[j - 1] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
